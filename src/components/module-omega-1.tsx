cb1521f02074f69e0d7ac28a4cab71afb23024ca
Dan <toloczkocoutinho@gmail.com>
2025-09-11 14:06:04 +0000

    Módulo Omega 1: src/components/module-omega-1.tsx
    typescript
    'use clien

---
+++ src/components/module-omega-1.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import ModuleOmega from './module-omega';
+
+export default function ModuleOmega1() {
+  return (
+    <ModuleOmega moduleId={1} moduleType="temporal_manipulation">
+      <div className="module-omega-1">
+        <h1>Omega 1: Manipulação Temporal</h1>
+        <p>Controle sobre o fluxo do tempo e realidades paralelas.</p>
+        {/* Interface específica do módulo 1 */}
+      </div>
+    </ModuleOmega>
+  );
+}
