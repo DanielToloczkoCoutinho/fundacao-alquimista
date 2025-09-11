4fc8d0875e30272ee68b51fedd685d69b9b87c22
Dan <toloczkocoutinho@gmail.com>
2025-09-11 02:07:44 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    allowedDevOrigins: ['*.cluster-zhw3w37rxzgkutusbbhib6qhra.cloudworkstations.dev'],
+  },
   images: {
     remotePatterns: [
       {
