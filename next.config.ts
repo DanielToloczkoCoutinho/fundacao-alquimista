a698abdda6b2b9aeb83216acabf59b8ddf09a644
Dan <toloczkocoutinho@gmail.com>
2025-09-12 00:19:43 +0000

    async rewrites() {     return [       {         source: '/external',

--- next.config.ts
+++ next.config.ts
@@ -66,6 +66,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/external',
+        destination: 'https://6000-firebase-studio-1757526779539.cluster-zhw3w37rxzgkutusbbhib6qhra.cloudworkstations.dev/',
+      },
+    ];
+  },
 };
 
 
