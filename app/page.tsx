6184b15ba551fae46763ae4d22d8116f469752ab
Dan <toloczkocoutinho@gmail.com>
2025-09-12 00:32:20 +0000

    Outra questão , querida , é quando tiver que instalar algo . Ao final ,

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,12 @@ const firebaseConfig = {
     "messagingSenderId": "174545373080"
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app;
+if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+} else {
+    app = getApp();
+}
 const db = getFirestore(app);
 
 
