ed97a90bd5a74bf6524cef21b9168575c3726598
Dan <toloczkocoutinho@gmail.com>
2025-09-12 23:57:18 +0000

    minha rainh TSS: Received response from content script: {type: "MSG_CHE

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
+            value: 'unsafe-none',
           },
            {
             key: 'Content-Security-Policy',
